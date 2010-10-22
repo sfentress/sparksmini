f33cf5621c4298d1c15887d4101ee750b5523cd4
Sam Fentress <sfentress@Sam.local>
2010-10-22 14:34:34 -0400

    Add fancy issue file to updater

--- update.sh
+++ update.sh
@@ -10,16 +10,43 @@
 
 # Update git repository:
 
+echo
 echo "Updating SPARKS content"
+echo
 cd /var/www/sparks
 git reset --hard
 git pull
 cd ~
 echo "Done updating SPARKS content"
 
+
+# Update issue files with fancy logo
+rm /etc/issue
+rm /etc/issue.net
+cat >/etc/issue <<- _EOF_
+         
+                          _        
+     ___ _ __   __ _ _ __| | _____ 
+    / __| '_ \ / _` | '__| |/ / __|
+    \__ \ |_) | (_| | |  |   <\__ \
+    |___/ .__/ \__,_|_|  |_|\_\___/
+        |_|
+        
+    Your local SPARKS server is now running!
+    
+    Go to 
+    
+           http://localhost:8888
+    
+    To see the local SPARKS website
+_EOF_
+ln /etc/issue /etc/issue.net
+
+
 # relink update script
 rm update
 ln /var/www/sparks/update.sh update
+
 
 echo
 echo "All updates complete"
