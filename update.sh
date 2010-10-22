fb9ae78c412a74fc705cbe15e223f8da63c783f8
Sam Fentress <sfentress@Sam.local>
2010-10-22 13:58:40 -0400

    Add update shell script, for simple updating of server

---
+++ update.sh
@@ -0,0 +1,18 @@
+#!bin/bash
+#
+# This is the update script for the SPARKS local server. It should be symbolically linked to the user's home directory
+# by typing
+#    ln -s update.sh ~/update
+#
+# From now on, the user can run the update script directly after logging in, by simply typing
+#    . update
+
+
+# Update git repository:
+
+echo "Updating SPARKS content"
+cd /var/www/sparks
+git reset --hard
+git pull
+cd ~
+echo "Done updating SPARKS content"
